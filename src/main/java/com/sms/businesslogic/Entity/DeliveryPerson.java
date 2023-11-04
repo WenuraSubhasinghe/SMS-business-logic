fa6a6892ea003431e8da2592ae470d91892f4f17
rish-99 <risinimethmini@gmail.com>
2023-11-04 23:37:53 +0530

    added getters and setters to deliveryperson

--- src/main/java/com/sms/businesslogic/Entity/DeliveryPerson.java
+++ src/main/java/com/sms/businesslogic/Entity/DeliveryPerson.java
@@ -1,15 +1,54 @@
 package com.sms.businesslogic.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
 import javax.xml.transform.sax.SAXResult;
 
 @Entity
+@NoArgsConstructor
 public class DeliveryPerson {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) //PK generation automatically, unique ids
     private long id;
     private String name;
     private String phoneNumber;
     private String vehicleNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+
 }
