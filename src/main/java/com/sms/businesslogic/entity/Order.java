b1e8cfb63c84c90110ea547dbfe849557e95186a
salinda-yasas-99 <salindasym@gmail.com>
2023-11-05 00:11:30 +0530

    resolving errors

--- src/main/java/com/sms/businesslogic/entity/Order.java
+++ src/main/java/com/sms/businesslogic/entity/Order.java
@@ -1,41 +1,21 @@
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Entity/Order.java
-package com.sms.businesslogic.Entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-=======
 package com.sms.businesslogic.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
->>>>>>> main:src/main/java/com/sms/businesslogic/entity/Order.java
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Entity/Order.java
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "orders")
-public class Order {
-
-=======
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "_order")
+@Table(name = "t_order")
 public class Order {
->>>>>>> main:src/main/java/com/sms/businesslogic/entity/Order.java
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
@@ -46,17 +26,6 @@ public class Order {
     private String orderStatus;
     private String deliveryStatus;
 
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Entity/Order.java
-   /* @OneToOne(mappedBy = "reserve")
-    private Courier courier;
-
-    @ManyToOne
-    @JoinColumn(name = "userId")
-    @JsonBackReference
-    private User user;*/
-
-
-=======
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "userId")
     private User user;
@@ -75,7 +44,6 @@ public class Order {
 //    @JoinColumn(name = "userId")
 //    @JsonBackReference
 //    private User user;
->>>>>>> main:src/main/java/com/sms/businesslogic/entity/Order.java
 //    @ManyToMany(fetch =FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
 //    @JoinTable(
 //            name = "reserve_book",
@@ -84,22 +52,11 @@ public class Order {
 //            inverseJoinColumns = {@JoinColumn(name = "bookId")}
 //    )
 //    private Set<Book> books=new HashSet<>();
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Entity/Order.java
-
-=======
->>>>>>> main:src/main/java/com/sms/businesslogic/entity/Order.java
 //    public void addBook(Book book){
 //        this.books.add(book);
 //        book.getReserves().add(this);
 //    }
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Entity/Order.java
-
-/*    @OneToMany(mappedBy="reserve",cascade = CascadeType.ALL)
-    @JsonManagedReference
-    private Set<ReserveBook> reserveBooks=new HashSet<>();*/
-=======
 //    @OneToMany(mappedBy="reserve",cascade = CascadeType.ALL)
 //    @JsonManagedReference
 //    private Set<ReserveBook> reserveBooks=new HashSet<>();
->>>>>>> main:src/main/java/com/sms/businesslogic/entity/Order.java
 }
--- src/main/java/com/sms/businesslogic/repository/OrderRepository.java
+++ src/main/java/com/sms/businesslogic/repository/OrderRepository.java
@@ -1,13 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/sms/businesslogic/Repository/OrderRepository.java
-package com.sms.businesslogic.Repository;
-
-import com.sms.businesslogic.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-
-}
-=======
 package com.sms.businesslogic.repository;
 
 import com.sms.businesslogic.entity.Order;
@@ -17,4 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
->>>>>>> main:src/main/java/com/sms/businesslogic/repository/OrderRepository.java
