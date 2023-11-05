689aeb63a496d3ec0ddd4bbb977002f4894d59ea
lakshankd <dominiclakshank@gmail.com>
2023-11-05 20:06:59 +0530

    changing the controller class name.

--- src/main/java/com/sms/businesslogic/controller/PaymentController.java
+++ src/main/java/com/sms/businesslogic/controller/PaymentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/payment")
 @RequiredArgsConstructor
-public class StripePaymentController {
+public class PaymentController {
 
     @Value("${stripe.apikey}")
     String stripeKey;
