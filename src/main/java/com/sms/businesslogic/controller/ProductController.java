ab0b1cc129f93446a7eb1c3db7340d4e24760173
salinda-yasas-99 <salindasym@gmail.com>
2023-11-07 12:31:41 +0530

    debugging

--- src/main/java/com/sms/businesslogic/controller/ProductController.java
+++ src/main/java/com/sms/businesslogic/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.sms.businesslogic.controller;
 
+import com.sms.businesslogic.dto.ProductDTO;
 import com.sms.businesslogic.entity.Product;
 import com.sms.businesslogic.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,51 +12,67 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    @GetMapping("/getProducts")
+    public List<ProductDTO> getAllProducts() {
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
-        return new ResponseEntity<>(products, HttpStatus.OK);
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProductById(@PathVariable Integer productId) {
-        Product product = productService.getProductById(productId);
-        if (product != null) {
-            return new ResponseEntity<>(product, HttpStatus.OK);
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Integer productId) {
+        ProductDTO productDTO = productService.getProductById(productId);
+        if (productDTO != null) {
+            return new ResponseEntity<>(productDTO, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.createProduct(product);
-        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    public ResponseEntity<?> createProduct(@RequestBody ProductDTO productDTO) {
+       try {
+           productService.createProduct(productDTO);
+           return  ResponseEntity.ok("Product saved successfully");
+       }
+       catch (Exception e){
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                   .body("Error saving the product: "+e.getMessage());
+       }
     }
 
-    @PutMapping("/{productId}")
-    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId, @RequestBody Product product) {
-        Product updatedProduct = productService.updateProduct(productId, product);
-        if (updatedProduct != null) {
-            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+  @PutMapping("/{productId}")
+    public ResponseEntity<?> updateProduct(@PathVariable Integer productId, @RequestBody ProductDTO productDTO) {
+      try {
+          productService.updateProduct(productId,productDTO);
+          return  ResponseEntity.ok("Product updatd successfully");
+      }
+      catch (Exception e){
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                  .body("Error saving the product: "+e.getMessage());
+      }
     }
 
-    @DeleteMapping("/{productId}")
+/*    @DeleteMapping("/{productId}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Integer productId) {
         productService.deleteProduct(productId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }*/
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Integer productId) {
+        String deletionMessage = productService.deleteProduct(productId);
+
+        if ("Delete successful".equals(deletionMessage)) {
+            return new ResponseEntity<>(deletionMessage, HttpStatus.OK);
+        } else if (deletionMessage.startsWith("Product with ID")) {
+            return new ResponseEntity<>(deletionMessage, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(deletionMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/sms/businesslogic/service/ProductService.java
+++ src/main/java/com/sms/businesslogic/service/ProductService.java
@@ -1,45 +1,94 @@
 package com.sms.businesslogic.service;
 
+import com.sms.businesslogic.dto.ProductDTO;
+import com.sms.businesslogic.entity.Category;
 import com.sms.businesslogic.entity.Product;
+import com.sms.businesslogic.repository.CategoryRepository;
 import com.sms.businesslogic.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    private final CategoryRepository categoryRepository;
+
+
+    public ProductDTO productToProductDTO(Product product) {
+        return ProductDTO.builder()
+                .productName(product.getProductName())
+                .quantity(product.getQuantity())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .categoryId(product.getCategory().getCategoryId()) // Assuming category ID is part of Product entity
+                .build();
     }
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(this::productToProductDTO)
+                .collect(Collectors.toList());
     }
 
-    public Product getProductById(Integer productId) {
+    public ProductDTO getProductById(Integer productId) {
         Optional<Product> optionalProduct = productRepository.findById(productId);
-        return optionalProduct.orElse(null);
+        return optionalProduct.map(this::productToProductDTO).orElse(null);
     }
 
-    public Product createProduct(Product product) {
-        return productRepository.save(product);
+    public void createProduct(ProductDTO productDTO) {
+
+        Category category= categoryRepository.findById(productDTO.getCategoryId())
+                .orElseThrow(() -> new IllegalArgumentException("Category not found"));
+
+        Product product = new Product();
+        product.setProductName(productDTO.getProductName());
+        product.setQuantity(productDTO.getQuantity());
+        product.setPrice(productDTO.getPrice());
+        product.setDescription(productDTO.getDescription());
+        product.setCategory(category);
+        productRepository.save(product);
     }
 
-    public Product updateProduct(Integer productId, Product updatedProduct) {
-        if (productRepository.existsById(productId)) {
-            updatedProduct.setProductId(productId);
-            return productRepository.save(updatedProduct);
+    public void updateProduct(Integer productId, ProductDTO productDTO) {
+
+        Category category= categoryRepository.findById(productDTO.getCategoryId())
+                .orElseThrow(() -> new IllegalArgumentException("Category not found"));
+
+        Product product= productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        if (product!=null && category!=null) {
+            product.setProductName(productDTO.getProductName());
+            product.setQuantity(productDTO.getQuantity());
+            product.setPrice(productDTO.getPrice());
+            product.setDescription(productDTO.getDescription());
+            product.setCategory(category);
+            productRepository.save(product);
+
         }
-        return null; // or throw an exception indicating the product doesn't exist
     }
 
-    public void deleteProduct(Integer productId) {
+/*    public void deleteProduct(Integer productId) {
         productRepository.deleteById(productId);
+    }*/
+
+    public String deleteProduct(Integer productId) {
+        try {
+            productRepository.deleteById(productId);
+            return "Delete successful";
+        } catch (EmptyResultDataAccessException ex) {
+            return "Product with ID " + productId + " does not exist";
+        } catch (Exception e) {
+            return "Error deleting product: " + e.getMessage();
+        }
     }
 }
